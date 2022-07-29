d1c63a3a9016f65f680bf580171289ec21275a0b
Vivekanand R <75977224+vivek-unni@users.noreply.github.com>
2022-07-29 20:32:50 +0530

    changes to pay

--- assets/js/new-pay.js
+++ assets/js/new-pay.js
@@ -14,23 +14,21 @@ function logout(){
     location.replace("./");
 }document.getElementById('logout').addEventListener("click",logout);
 const form = document.getElementById('form')
-var gender="";
-document.getElementById('gender_0').addEventListener("click",()=>{gender=document.getElementById('gender_0').value;})
-document.getElementById('gender_1').addEventListener("click",()=>{gender=document.getElementById('gender_1').value;})
+
 form.onsubmit=(e)=>{
     console.log(e)
     e.preventDefault();
-    var username=document.getElementById('username').value;
-    var empname = document.getElementById('empname').value; 
-    var uid = document.getElementById('uid').value; 
-    var pass = document.getElementById('pass').value; 
-    var empid = document.getElementById('empid').value; 
-    var age = document.getElementById('age').value; 
-    var dob = document.getElementById('dob').value; 
-    // var gender = document.getElementById('gender').value; 
-    var doj = document.getElementById('doj').value; 
-    var desig = document.getElementById('desig').value; 
-    var ofc = document.getElementById('ofc').value; 
+    var pid=document.getElementById('pid').value;
+    var uid = document.getElementById('uid').value;
+    var empname = document.getElementById('empname').value;  
+    var paydate = document.getElementById('paydate').value; 
+    var basicpay = document.getElementById('basicpay').value; 
+    var da = document.getElementById('da').value; 
+    var hra = document.getElementById('hra').value; 
+    var cca = document.getElementById('cca').value; 
+    var inslic = document.getElementById('inslic').value; 
+    var sli = document.getElementById('sli').value; 
+    var gpf = document.getElementById('gpf').value; 
     var xtp = new XMLHttpRequest();
     xtp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -41,5 +39,5 @@ form.onsubmit=(e)=>{
     // xtp.open("POST", "http://localhost/Server/new-emp.php", true);
     // xtp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     
-    xtp.send(`username=${username}&empname=${empname}&uid=${uid}&pass=${pass}&empid=${empid}&age=${age}&dob=${dob}&gender=${gender}&doj=${doj}&desig=${desig}&ofc=${ofc}`);
+    xtp.send(`pid=${pid} & uid=${uid} & empname=${empname} & paydate=${paydate} & basicpay=${basicpay} & da=${da} & hra=${hra} & cca=${cca} & inslic=${inslic} & sli=${sli} & gpf=${gpf}`);
 }
