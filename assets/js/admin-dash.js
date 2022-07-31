46ac4c2e34b3fe5fc6cffb8e4249487f80df4e7d
Tomin Joy <tominjk007@gmail.com>
2022-07-31 21:03:23 +0530

    bug fix

--- assets/js/admin-dash.js
+++ assets/js/admin-dash.js
@@ -13,7 +13,7 @@ function logout(){
     document.cookie = 'token=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     location.replace("./");
 }
-
+document.getElementById('logout').addEventListener("click",logout);
 document.getElementById('add-emp').addEventListener("click",()=>{
     document.getElementById('new-emp-form').classList.remove('d-none');
     
